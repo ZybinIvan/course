88152aef2a351d3d6081a6bdf76a4b8ef6555d2e
IvanZybin <iivan.zybin@gmail.com>
2022-02-06 20:37:14 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/laba5b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba5b.dir/main.c.obj"
+  "laba5b.exe"
+  "laba5b.exe.manifest"
+  "laba5b.pdb"
+  "liblaba5b.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/laba5b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
