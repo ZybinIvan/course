9de3c8425150f52c5e70613ce4abae92cef16100
IvanZybin <iivan.zybin@gmail.com>
2022-03-01 12:04:35 +0300

    is equality \ string

--- libs/string/string_.c
+++ libs/string/string_.c
@@ -8,21 +8,21 @@ size_t strlen_(const char *begin) {
     return end - begin;
 }
 
-char* find(char *begin, char *end, int ch) {
+char *find(char *begin, char *end, int ch) {
     while (begin != end && *begin != ch)
         begin++;
 
     return begin;
 }
 
-char* findNonSpace(char *begin) {
+char *findNonSpace(char *begin) {
     while (isspace(*begin) && *begin != '\0')
         begin++;
 
     return begin;
 }
 
-char* findSpace(char *begin) {
+char *findSpace(char *begin) {
     while (!isspace(*begin) && *begin != '\0')
         begin++;
 
@@ -41,4 +41,13 @@ char *findSpaceReverse(char *rbegin, const char *rend) {
         rbegin--;
 
     return rbegin;
+}
+
+int strcmp(const char *lhs, const char *rhs) {
+    while (*lhs && *rhs && *lhs == *rhs) {
+        lhs++;
+        rhs++;
+    }
+
+    return *lhs - *rhs;
 }
--- libs/string/string_.h
+++ libs/string/string_.h
@@ -6,14 +6,16 @@
 
 size_t strlen_(const char *begin);
 
-char* find(char *begin, char *end, int ch);
+char *find(char *begin, char *end, int ch);
 
-char* findNonSpace(char *begin);
+char *findNonSpace(char *begin);
 
-char* findSpace(char *begin);
+char *findSpace(char *begin);
 
-char* findNonSpaceReverse(char *rbegin, const char *rend);
+char *findNonSpaceReverse(char *rbegin, const char *rend);
 
-char* findSpaceReverse(char *rbegin, const char *rend);
+char *findSpaceReverse(char *rbegin, const char *rend);
+
+int strcmp(const char *lhs, const char *rhs);
 
 #endif //LABA5B_5D_STRING__H
